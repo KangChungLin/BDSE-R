4bc2263729f18d3e435ac1e7a39acc155a9fbd8e
KangChungLin <66732309+KangChungLin@users.noreply.github.com>
2020-08-24 10:44:39 +0800

    Update read_files&import_to_db.R

--- read_files&import_to_db.R
+++ read_files&import_to_db.R
@@ -1,17 +1,18 @@
 library(readr)
 # read 50 files one time
-files = list.files('after_r4/' , pattern = '.csv' )
+files <- list.files('tf_table/' , pattern = '.csv' )
 # 50 files to one list
-tables = lapply(paste("after_r4",files,sep="/"),read_csv)
+tables <- lapply(paste("tf_table",files,sep="/"),read_csv)
 # rename table names of the list
-newname = gsub('_r.csv', '', files)
-names(tables) = newname
+newname <- gsub('.csv', '', files)
+newname <- paste0('stock',newname)
+names(tables) <- newname
 
 # connect to mariadb, use database "treering"
 library(RMariaDB)
 library(DBI)
 drv <- dbDriver("MariaDB")
-con <- dbConnect(drv, username="root", password="", dbname ="treering", host="localhost")
+con <- dbConnect(drv, username="root", password="", dbname ="stock", host="localhost")
 
 # import tables to database
 mapply(dbWriteTable, name = newname, value = tables , MoreArgs = list(conn = con))
